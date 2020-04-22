d694fea9dfd5b0850098d8e18895660e1971ddba
zrahat <zarifrahat123@gmail.com>
2020-04-22 07:21:02 -0400

    updates

--- Build Your Own ActiveRecord/lib/00_attr_accessor_object.rb
+++ Build Your Own ActiveRecord/lib/00_attr_accessor_object.rb
@@ -3,8 +3,11 @@ class AttrAccessorObject
     # ...
     names.each do |name|
       define_method(attr_accessor) {  
-      instance_variable_get(name)
+      instance_variable_get("@#{name}")
       }
+
+      define_method("#{name}=") do |arg|
+        instance_variable_set("@#{name}", arg)
     end
   end
 end
--- Build Your Own ActiveRecord/lib/01_sql_object.rb
+++ Build Your Own ActiveRecord/lib/01_sql_object.rb
@@ -23,12 +23,12 @@ class SQLObject
 
   def self.table_name=(table_name)
     # ...
-    table_name
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
-    "#{self.to_s.tableize}"
+    @table_name ||= "#{self.to_s.tableize}"
   end
 
   def self.all
